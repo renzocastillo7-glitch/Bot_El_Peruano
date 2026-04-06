aa624bf4241d810f115413cf28a58b6f3da56688
renzocastillo7-glitch <renzo.castillo7@gmail.com>
2026-04-05 23:34:53 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -163,7 +163,7 @@ def main():
         
         # PRUEBA FORZADA 2: RESOLUCION PEI WEB CON LINK VERDADERO
         all_docs.append({
-            "text": "RESOLUCION N° 000049-2026/SUNAT. Fecha: 31/03/2026. Aprueban el programa para el envío de información a la SUNAT denominado PEI WEB que reemplaza al programa de envío de información a que se refiere la Resolución de Superintendencia N° 159-2017/SUNAT y establece la obligación de informar a través del nuevo programa dos documentos autorizados",
+            "text": "RESOLUCION N° 000049-2026/SUNAT. Fecha: 31/03/2026. Aprueban el programa para el envío de información a la SUNAT denominado PEI WEB que reemplaza al programa de envío de información a que se refiere la Resolución de Superintendencia N° 159-2017/SUNAT y establece la obligación de informar a través del nuevo programa dos documentos autorizados. DISPOSICIONES COMPLEMENTARIAS FINALES. Primera.- Vigencia. La presente resolución entra en vigor el 1 de agosto de 2026.",
             "url": "https://busquedas.elperuano.pe/dispositivo/NL/2500833-1",
             "source": "El Peruano - Normas Legales",
             "type": "norma"
